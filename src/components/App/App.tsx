a5e55b4abfb2ecef5e184b95e20ad68051d6d840
Stanislav Zavadskyi <101670941+uncle-Stas@users.noreply.github.com>
2023-02-28 12:59:43 +0200

    start project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, dolores vero aliquam
+      mollitia voluptas nisi quisquam at animi itaque quis culpa nihil cum soluta eveniet maiores!
+      Blanditiis, corporis deserunt! Dignissimos!
+    </div>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import '@csstools/normalize.css'; /* bring in normalize.css styles */
+import './index.css'; /* reset default styles */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
